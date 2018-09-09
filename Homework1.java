150f42c9291718aab96c9cb0b35a8d512544dadf
Rudolph <rudoljus@berkeleyprep.org>
2018-09-09 14:09:33 -0400

    Hopefully i did it the right way :)

--- Homework1.java
+++ Homework1.java
@@ -1,6 +1,6 @@
 /*
  * First and Last Names
- */
+ /* Justin Rudolph*/
 
 import java.awt.Rectangle;
 
@@ -8,36 +8,44 @@ public class Homework1 {
 
 	/* Fix the error(s) in this method so that it runs properly
 	 */
-	public static int problem1() {
-		int mystery = 1;
-		mystery = mystery + 1;
-		int mystery = 1 - 2 * mystery;
-		return mystery;
+	 public static int problem1() {
+ 		int mystery = 1;
+ 		mystery = mystery + 1;
+ 		mystery = 1 - 2 * mystery;
+ 		return mystery;
+
 	}
 
 	/* Construct and return a Rectangle that has a width of w and a
 	 * height of h
 	 */
 	public static Rectangle problem2(int w, int h) {
+Rectangle rect = new Rectangle (w, h);
+return rect;
+
 
 	}
 
 	/* Declare an integer, assign it a value of 5, then return it
 	 */
 	public static int problem3() {
+int number = 5;
+return number;
 
 	}
 
 	/* Declare and return a String containing the phrase "Computer Science"
 	 */
 	public static String problem4() {
-
+String phrase = ("Computer Science");
+return phrase;
 	}
 
 	/* Add 5 to num and return the new value
 	 */
 	public static int problem5(int num) {
-
+num = num + 5;
+return num;
 	}
 
 }
